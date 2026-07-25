7677a79b37dc0c02ec7b22a89360860bdc0ac26e
IT01 <acp.it01@anphatplastic.com>
2026-07-25 08:07:15 +0700

    fdfsd

--- FE/app/pages/index.vue
+++ FE/app/pages/index.vue
@@ -3,12 +3,15 @@
  * Trang chủ tạm — Redirect về trang đăng nhập / admin.
  * (Sẽ được thay thế bằng Landing Page sau này)
  */
-definePageMeta({
-  middleware: [
-    function () {
-      const token = useCookie('auth_token')
-      return navigateTo(token.value ? '/admin' : '/login')
-    }
-  ]
-})
+// definePageMeta({
+//   middleware: [
+//     function () {
+//       const token = useCookie('auth_token')
+//       return navigateTo(token.value ? '/admin' : '/login')
+//     }
+//   ]
+// })
 </script>
+<template>
+  <div>hello</div>
+</template>
